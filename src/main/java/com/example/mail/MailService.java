e1d6eb0930562afbd156c0b37570cb9d3053e18d
zdb <2271738697@qq.com>
2020-03-16 00:17:04 +0800

    邮件合并

--- src/main/java/com/example/mail/MailService.java
+++ src/main/java/com/example/mail/MailService.java
@@ -1,9 +1,16 @@
 package com.example.mail;
 
+import javafx.scene.media.MediaException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 /**
  * @author zdb
@@ -14,6 +21,7 @@ public class MailService {
     @Autowired
     JavaMailSender javaMailSender;
 
+    /*普通文本内容发送*/
     public void sendSimpleMail(String from, String to, String cc,
                                String subject, String content) {
         SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
@@ -30,6 +38,51 @@ public class MailService {
         javaMailSender.send(simpleMailMessage);
 
     }
+    /*附加文本内容*/
+    public void sendAttachFileMail(String from, String to, String subject,
+                                   String content, File file) {
+        try {
+            MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+            //这里的true表示构建一个multipart类型的邮件
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage,true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content);
+            helper.addAttachment(file.getName(),file);
+            javaMailSender.send(mimeMessage);
+        } catch (MediaException | MessagingException e) {
+            e.printStackTrace();
+        }
 
+
+    }
+
+    //带图片
+    public void sendMailWithImg(String from, String to, String subject,
+                                String content,String[] srcPath,String[] resIds ){
+        if (srcPath.length != resIds.length) {
+            System.out.println("发送失败！");
+            return;
+        }
+        try {
+            MimeMessage message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content);
+            for (int i = 0; i < srcPath.length; i++) {
+                //获取资源路径
+                FileSystemResource res = new FileSystemResource(new File(srcPath[i]));
+                helper.addInline(resIds[i],res);
+            }
+            javaMailSender.send(message);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
--- src/test/java/com/example/mail/MailApplicationTests.java
+++ src/test/java/com/example/mail/MailApplicationTests.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+
 @SpringBootTest
 class MailApplicationTests {
 
@@ -12,12 +14,30 @@ class MailApplicationTests {
 
     @Test
     void contextLoads() {
-        mailSrervice.sendSimpleMail("2271738697@qq.com",
+        /*发送人*/
+        String from ="2271738697@qq.com";
+        /*收件人*/
+        String to ="1050881627@qq.com";
+        /*c抄写人*/
+        String subject ="1050881627@qq.com";
+        /*主题*/
+        String content ="测试主题";
+
+
+      /*  mailSrervice.sendSimpleMail("2271738697@qq.com",
                 "1050881627@qq.com",
                 "1050881627@qq.com",
                 "测试主题",
                 "内容");
-        System.out.println("成功");
+        System.out.println("成功");*/
+
+        //测试附带文本
+        mailSrervice.sendAttachFileMail(from,to,subject,content,new File("C://Users/Administrator/Desktop/已完成任务/返利功能.txt"));
+
+        //测试上传图片
+        mailSrervice.sendMailWithImg(from,to,"这是图片邮件","<div>hello,这是一封带图片的邮件"+
+                "这图片是：<div><img src='cid:po1'/></div></div>",new String[]{"C://Users/Administrator/Pictures/bug.jpg"},new String[]{"p01"});
+
     }
 
 }
